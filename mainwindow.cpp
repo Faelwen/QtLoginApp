44f69b82272c7f75e1c8f83402ae00983d7ca79c
Faelwen <francis.john.mccarthy@googlemail.com>
2017-10-28 21:56:54 +0200

    Logic

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,4 +18,10 @@ void MainWindow::on_pushButton_login_clicked()
 {
     QString username = ui->lineEdit_username->text();
     QString password = ui->lineEdit_password->text();
+
+    if(username == "test" && password == "test") {
+        QMessageBox::information(this, "Login", "Username and passord is correct");
+    } else {
+        QMessageBox::warning(this, "Login", "Username or password is not correct");
+    }
 }
